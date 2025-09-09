8ed4ed2c1293ec43c4f85ff13fc5d45f6634871c
Mfranklin06 <marconifilho2022@gmail.com>
2025-09-09 18:38:15 -0300

    feat: change the way to get data

--- src/app/api/salas/route.ts
+++ src/app/api/salas/route.ts
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
+import { getSalasData } from "@/lib/data";
 
-const salas = [
-  { id: 1, name: "Sala 1", status: "aberto", temperatura: 22, raw: [] },
-  { id: 2, name: "Sala 2", status: "fechado", temperatura: 25, raw: [] },
-  { id: 3, name: "Sala 3", status: "aberto", temperatura: 20, raw: [] },
-];
 export async function GET() {
+  const salas = getSalasData();
   return NextResponse.json(salas);
 }
