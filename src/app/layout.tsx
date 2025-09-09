9dec495ea5ed964ebf0b786f1bf0f825cb68ddc6
Mfranklin06 <marconifilho2022@gmail.com>
2025-09-09 17:45:45 -0300

    feat: add ThemeRegistry component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import ThemeRegistry from "./components/themeRegistry";
 
 export const metadata: Metadata = {
   title: "RAISE",
@@ -16,7 +16,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="min-h-screen flex flex-col">
         <main className="bg-[#0e0c26] flex-1">
-          {children}
+          <ThemeRegistry>
+            {children}
+          </ThemeRegistry>
         </main>
       </body>
     </html>
