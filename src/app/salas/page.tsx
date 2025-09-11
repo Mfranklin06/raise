6242b24c24072f2f9683b219686eb7291099c408
Mfranklin06 <marconifilho2022@gmail.com>
2025-09-11 07:57:29 -0300

    feat: add salas page

---
+++ src/app/salas/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SalasPage = () => {
+    return (
+        <div>
+            <h1>Salas</h1>
+            <p>Bem-vindo à página de Salas!</p>
+        </div>
+    );
+};
+
+export default SalasPage;
