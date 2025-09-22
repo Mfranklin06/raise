f119fffa0c058aac43bbfbdd1e5f91675da09db0
Mfranklin06 <marconifilho2022@gmail.com>
2025-09-22 12:50:20 -0300

    feat: add salaCard in id/page

--- src/app/salas/[id]/page.tsx
+++ src/app/salas/[id]/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-    return <div>Detalhes da Sala</div>;
+import { SalaCards } from "@/app/components/SalaCards";
+import { getUnidadesAC, UnidadeAC } from "@/lib/data";
+
+async function getUnidade(params: { id: string }): Promise<UnidadeAC> {
+    const unidades = await getUnidadesAC();
+    return unidades[parseInt(params.id) - 1]; // Ajuste simples, mas idealmente buscar por ID
+}
+interface SalaProps {
+    params: {
+        id: string;
+    };
+}
+
+export default async function Sala(u: SalaProps) {
+    const unidades = await getUnidade(u.params);
+    return <div><SalaCards unidades={unidades}/></div>;
 }
