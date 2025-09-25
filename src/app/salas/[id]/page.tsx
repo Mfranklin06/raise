cbe0bb3c09560d897288bb04ed3b7802215050cd
Mfranklin06 <marconifilho2022@gmail.com>
2025-09-25 10:04:07 -0300

    feat: add padding top

--- src/app/salas/[id]/page.tsx
+++ src/app/salas/[id]/page.tsx
@@ -20,7 +20,7 @@ export default async function Sala(u: SalaProps) {
     // The component now receives the correctly typed action.
     return (
         <main className="min-h-screen bg-background">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-[5rem]">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-[5rem] pt-[5rem]">
                 <SalaCards unidade={unidades} updateAction={updateEstadoAction} />
             </div>
         </main>
